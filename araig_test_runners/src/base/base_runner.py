4c912e77d7ae6b8008db93ec5bb3da93d2dff0ce
ipa-rwu <ruichao.wu@ipa.fraunhofer.de>
2021-02-04 22:45:32 +0100

    base_runner: noblock, check condition, rosparam

--- araig_test_runners/src/base/base_runner.py
+++ araig_test_runners/src/base/base_runner.py
@@ -6,7 +6,7 @@ import os
 import threading
 
 class TestBase(object):
-    def __init__(self, sub_dict = {} , pub_dict = {}, rate = 100):      
+    def __init__(self, sub_dict = {} , pub_dict = {}, param_list = {}, rate = 100):      
 
         self._rate = rospy.Rate(rate)
 
@@ -23,13 +23,20 @@ class TestBase(object):
             "test_failed"           : "/signal/runner/test_failed",
             "test_succeeded"        : "/signal/runner/test_succeeded",
             "start_test"            : "/signal/ui/start_test",
-            "reset_test"            : "/signal/ui/reset_test"
+            "reset_test"            : "/signal/ui/reset_test",
+            "interrupt_test"        : "/signal/ui/interrupt_test"
         }
         self._output_interface.update(pub_dict)
+        self._config_param = []
+        self._config_param += param_list
 
         self._locks = {}
         self._flag = {}
         self._publishers = {}
+
+        # get ros param:
+        self.config_param = {}
+        self.get_config(self._config_param)
 
         # sub_init
         for key in self._input_interface:
@@ -54,17 +61,18 @@ class TestBase(object):
             return
         with self._locks[key]:
             self._flag[key] = value
-
-    def getSafeFlag(self, key, seq = False):
+    
+    # If seq = False, get data; If True, get header
+    def getSafeFlag(self, key, header = False):
         if not key in self._input_interface.keys():
             rospy.logerr(rospy.get_name() + ": Retrieving a key that does not exist!: {}".format(key))
             return
         else:
             with self._locks[key]:
-                if not seq:
+                if not header:
                     return self._flag[key].data
                 else:
-                    return self._flag[key].header.seq
+                    return self._flag[key].header
     
     def buildNewBoolStamped(self, data = True):
         msg = BoolStamped()
@@ -78,6 +86,26 @@ class TestBase(object):
     def startRecordingAndWait(self, duration=3):
         rospy.loginfo(rospy.get_name() + ": waiting for {}s, then start recoding ...".format(duration))
         rospy.sleep(duration)
+
+    def timestampToFloat(self, stamp):
+        timestamp_float =  float(stamp.secs + float(stamp.nsecs*(1e-9)))
+        return timestamp_float
+
+    def checkCondition(self, start_timestamp, duration):
+        if self.timestampToFloat(rospy.Time.now() - start_timestamp) <= duration:
+            return False
+        else:
+            return True
+    
+    def get_config(self, param_list):
+        for arg in param_list:
+            module_name = "/runner/"
+            ns = module_name
+            if rospy.has_param(ns + arg):
+                self.config_param[arg] = rospy.get_param(ns + arg)
+            else:
+                rospy.logerr("{}: {} param not set!!".format(ns, arg))
+                rospy.signal_shutdown()
     
     def main(self):
         pass
